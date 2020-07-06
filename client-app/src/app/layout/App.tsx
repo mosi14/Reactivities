1f95c34acaa5c277c39cea35e1cfee8028e8c764
mosasad <asadollahymostafa@gmail.com>
2020-07-07 01:34:31 +0430

    end of 13. Selecting an individual Activity

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { List, Container } from 'semantic-ui-react'
+import { IActivity } from '../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+
+const  App =()=> {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+
+  const handleSelectActivity =(id: string)=> {
+    setSelectedActivity(activities.filter(a=> a.id === id)[0])
+  }
+
+  useEffect(() => {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+    .then((responce) => {
+      setActivities(responce.data)
+    });
+
+  }, []);
+
+  return (
+    <Fragment> 
+        <NavBar/>
+        <Container style = {{marginTop: '7em'}}>
+          <ActivityDashboard 
+            activities= {activities}  
+            selectActivity= {handleSelectActivity}
+            selectedActivity= {selectedActivity}
+            />
+        </Container>
+    </Fragment>
+  );
+}
+
+export default App;
